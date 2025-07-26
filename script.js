042a69e979c3f06883256648563acbd9cc9ab341
Shubham277353 <golubhandari405@gmail.com>
2025-07-26 15:26:23 +0530

    feat: display numbers on screen

--- script.js
+++ script.js
@@ -14,26 +14,37 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
-function operate (no1,opt,no2){
-    let result;
-    if(opt==="+"){
-    result = add(no1,no2);
-    }
+function operate(no1, opt, no2) {
+  let result;
+  if (opt === "+") {
+    result = add(no1, no2);
+  } else if (opt === "-") {
+    result = subtract(no1, no2);
+  } else if (opt === "*") {
+    result = multiply(no1, no2);
+  } else if (opt === "/") {
+    result = divide(no1, no2);
+  }
 
-    else if(opt==="-"){
-    result = subtract(no1,no2);
-    }
-
-    else if(opt==="*"){
-    result = multiply(no1,no2);
-    }
-
-    else if(opt==="/"){
-    result = divide(no1,no2);
-    }
-
-    return result;
+  return result;
 }
 
-let number1 , number2 , operator;
-console.log(operate(5,"/",5));
+let number1, number2, operator , value;
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".numButtons");
+const equalTo = document.querySelector(".equalTo");
+
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if(btn.value !== "="){
+    display.textContent += btn.value;
+    value = btn.value;
+    }
+  });
+});
+
+equalTo.addEventListener("click", ()=>{
+    
+})
