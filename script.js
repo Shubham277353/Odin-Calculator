ceab995f1fc5e10c4b8ef206eca87d1f2f386cec
Shubham277353 <golubhandari405@gmail.com>
2025-07-27 17:15:05 +0530

    feat: add error on divide by 0 and back to back computation

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ const multiply = (num1, num2) => {
 };
 
 const divide = (num1, num2) => {
-  return num1 / num2;
+  if (num2 === "0") {
+    return "ERROR";
+  } else {
+    return num1 / num2;
+  }
 };
 
 let result = "";
@@ -26,7 +30,7 @@ function operate(no1, opt, no2) {
   } else if (opt === "/") {
     result = divide(no1, no2);
   }
-  console.log(result);
+  console.log(`result: ${result}`);
   return result;
 }
 
@@ -42,25 +46,49 @@ const symbol = document.querySelectorAll(".symbol");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
+    if (display.textContent === "ERROR") {
+          display.style.color = "white";
+      console.log("hello");
+      allClear();
+    }
     if (btn.value === "=") {
       return;
     } else if (["+", "-", "*", "/"].includes(btn.value) && operator === "") {
-      operator = btn.value;
-      display.textContent += ` ${btn.value} `;
-    } else if (btn.classList.contains("numbers") && operator !== "") {
+      if (number1 === "") {
+        allClear();
+      } else {
+        operator = btn.value;
+        display.textContent += ` ${btn.value} `;
+      }
+    } else if (
+      btn.classList.contains("numbers") &&
+      operator !== "" &&
+      number1 !== ""
+    ) {
       number2 += btn.value;
       display.textContent += btn.value;
     } else if (btn.classList.contains("numbers") && number2 === "") {
-      number1 += btn.value;
-      display.textContent += btn.value;
-    } else if (btn.classList.contains("numbers") && number2 !== "") {
-      console.log("number2 running");
-      number2 = btn.value;
-      display.textContent += number2;
-    } else if (["+", "-", "*", "/"].includes(btn.value) && operator !== "") {
-      console.log("helloworld");
-      operator = btn.value;
-      display.textContent += ` ${operator} `;
+        number1 += btn.value;
+        display.textContent += btn.value;
+    }
+
+    if (
+      btn.classList.contains("symbol") &&
+      operator !== "" &&
+      number1 !== "" &&
+      number2 !== ""
+    ) {
+      value = operate(number1, operator, number2);
+      if (value === "ERROR") {
+        display.textContent = "ERROR";
+      } else {
+        operator = btn.value;
+        display.textContent = "";
+        display.textContent += value;
+        display.textContent += ` ${btn.value} `;
+        number1 = value;
+        number2 = "";
+      }
     }
 
     console.log(number1);
@@ -71,8 +99,15 @@ buttons.forEach((btn) => {
 
 equalTo.addEventListener("click", () => {
   operatedNumber = operate(number1, operator, number2);
-  display.textContent = operatedNumber;
-  number1 = operatedNumber;
+  if (operatedNumber === "ERROR") {
+    display.textContent = "ERROR";
+    display.style.color = "red";
+  } else {
+    display.textContent = operatedNumber;
+    number1 = operatedNumber;
+    number2 = "";
+    operator = "";
+  }
 });
 
 function allClear() {
